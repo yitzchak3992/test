2dc883f0c55cf16325bb0a8b29b61a195bdae0f7
yitzchak <yitz3992@gmail.com>
2024-06-06 16:27:36 +0300

    Extracting INDEX to the main folder

--- js-piles/index.js
+++ js-piles/index.js
@@ -1,10 +1,22 @@
 const butLogin = document.querySelector("#login");
-const username = document.querySelector("#username").value;
-const password = document.querySelector("#password").value;
+const integrityWarning = document.querySelector("#integrityWarning");
 
 const login = () => {
   console.log(document.querySelector("#username").value);
   console.log(document.querySelector("#password").value);
+  const username = document.querySelector("#username").value.trim();
+  const password = document.querySelector("#password").value.trim();
+  if (!/[^a-zA-Zא-ת0-9\s]/.test(password)) {
+    integrityWarning.innerText = "הסיסמה צריכה להחיל תו מיחד כגון !@#$%^&*";
+    integrityWarning.style.display = "block";
+    return;
+  }
+  if (password.length < 6) {
+    integrityWarning.innerText = "יש להזין מינימום 6 תווים";
+    integrityWarning.style.display = "block";
+    return;
+  }
+  window.location.href = "html-pile/checkers.html";
 };
 
 butLogin.addEventListener("click", login);
